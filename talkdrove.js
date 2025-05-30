87c70eca53a874b57e95845b9b1a6feaab787e3c
newtonomukunda <newtonandala004@gmail.com>
2025-05-30 15:32:58 +0300

    .

--- talkdrove.js
+++ talkdrove.js
@@ -5,7 +5,7 @@
     "logo": "https://githubu.com/Charleskenya1/CHARLESKE",
     "keywords": ["bot"],
     "documentation-link": "https://session.talkdrove.com",
-    "owner-verification":"talkdrove@gmail.com",
+    "owner-verification":"hamaxdelalozi@gmail.com",
   
     "env": {
       "SESSION_ID": {
@@ -34,7 +34,7 @@
       },
       "OWNER_NUMBER": {
         "description": "OWNER_NUMBER with country code (e.g, 254759626063)",
-        "value":"254759626063",
+        "value":"254793753327",
         "required": true
         
       },
@@ -89,7 +89,7 @@
       },
       "PRESENCE": {
         "description": "Set presence of your WhatsApp, you can put online to keep your WhatsApp online for always, put offline to keep the WhatsApp normal, put typing to show that you are typing whenever anybody will message you! put recordig to show that you are recording whenever anybody will message you, or you can also show paused for nothing (e.g, online)",
-        "value":"oline",
+        "value":"typing",
         "required": false
         
       },
@@ -107,7 +107,7 @@
       },
       "PREMIUM_USERS": {
         "description": "There are some commands which can only be used by the premium users, put premium users number without +, multiple numbers can be seperated by comma (,) (e.g, 254759626063, or 923072380xx,9234538003xx)",
-        "value":"972",
+        "value":"254",
         "required": false
         
       }
